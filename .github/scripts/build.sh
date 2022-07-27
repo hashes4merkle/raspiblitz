1ac14d1b4d122a7f64f5bb76990a2ee61cfa069c
Deverick <deverick@apollolabs.io>
2022-07-26 19:33:37 -0500

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,8 +3,8 @@
 # Install packer
 echo -e "\nInstalling packer..."
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update -y && apt-get install packer -y
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update -y && sudo apt-get install packer -y
 
 # Install Packer Arm Plugin
 echo -e "\nInstalling Packer Arm Plugin..."
